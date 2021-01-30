66ca0047f61a1b607c0b4dde231d8cd43500f4f2
Walter Hulsebos <walterhulsebos@outlook.com>
2021-01-30 12:32:40 +0100

    Created Math Extensions

---
+++ Assets/-Source-/Scripts/Utilities/Extensions/MathExtensions.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public static class MathExtensions
+{
+	public static float Clamp(this float value, in float min, in float max)
+		=> Mathf.Clamp(value: value, min: min, max: max);
+	
+	public static float Clamp01(this float value)
+		=> Mathf.Clamp01(value: value);
+	
+	//public static float Lerp(this (float min, float max), in float t) => 
+}
