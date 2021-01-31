fe628cf53ee36bb06428ae9e4b6a6a18bd81ca88
Wybren <wybren.akker@gmail.com>
2021-01-31 16:24:12 +0100

    Finished targeting display

--- Assets/-Source-/Scripts/Game/Runtime/Misc/TargetDisplay.cs
+++ Assets/-Source-/Scripts/Game/Runtime/Misc/TargetDisplay.cs
@@ -12,7 +12,25 @@ namespace Game
 
         void Update()
         {
-            //targetDisplay = Targeter
+            if (PlayerController.Instance.TryGetComponent<Targeter>(out Targeter _targeter))
+            {
+                if(_targeter.TargetSprite != null)
+                {
+                    targetDisplay.gameObject.SetActive(true);
+                    targetDisplay.sprite = _targeter.TargetSprite;
+                    targetDisplay.color = _targeter.CurrentTarget.transform.GetComponent<Image>().color;
+                }
+                else
+                {
+                    targetDisplay.gameObject.SetActive(false);
+                }
+
+                
+            }
+            else
+            {
+                targetDisplay.gameObject.SetActive(false);
+            }
         }
     }
 }
